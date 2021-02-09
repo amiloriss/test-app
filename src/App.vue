8d298c8f6668451e87442ef0b22869270b6307e1
georgiy <33293661+amiloriss@users.noreply.github.com>
2021-02-09 15:47:39 +0300

    add axios and fetch data from API

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<Table v-if="tableData" :data="tableData" />
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Table from './components/Table';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: 'App',
+	components: {
+		Table,
+	},
+	data: () => ({
+		tableData: 10,
+	}),
+	async mounted() {
+		try {
+			const res = await this.$axios('/clients');
+			const data = res.data;
+			this.tableData = data;
+
+			const something = data.map(el => el.name);
+			console.log(something);
+		} catch (error) {
+			console.log(error.message);
+		}
+	},
+};
 </script>
 
 <style>
+* {
+	padding: 0;
+	margin: 0;
+	box-sizing: border-box;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
 }
 </style>
---
+++ src/components/Table.vue
@@ -0,0 +1,30 @@
+<template>
+
+	<table>
+        
+		<thead>
+            <th>
+                <td>Название клиента <br> Адрес</td>
+                <td>Первый из списка почтовый адрес</td>
+                <td>Первый из списка номер телефона</td>
+                <td>Дата создания</td>
+            </th>
+        </thead>
+		<tbody>
+            <tr v-for="(row, index) in data" :key="index">
+                <td v-for="(obj, index) in data" :key="index">{{row[obj.key]}}</td>
+            </tr>
+        </tbody>
+        {{data}}
+	</table>
+</template>
+
+<script>
+export default {
+    props: ['data']
+}
+</script>
+
+<style>
+    thead th td{width: 600px; padding: 20px 150px; background-color: rgb(158, 158, 158);}
+</style>
