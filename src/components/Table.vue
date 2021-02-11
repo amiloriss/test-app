6197d653da46947639fdf46cd2ac7555e2f13ec8
Georgii Tolparov <amiloriss@Georgiis-MacBook-Air.local>
2021-02-11 17:59:43 +0300

    add pagination

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,86 +1,121 @@
 <template>
-	<a-table
-		:columns="columns"
-		:data-source="dataClient"
-		:rowKey="record => record.id"
-	>
-		<span slot="customTitle"
-			>Название клиента <br />
-			Адресс
-		</span>
-	</a-table>
+  <a-table
+    :style="{ whiteSpace: 'pre' }"
+	:pagination="{defaultPageSize: 10, showSizeChanger: true, pageSizeOptions: ['10', '20', '30']}"
+    :columns="columns"
+    :data-source="dataClient"
+    :rowKey="(record) => record.id"
+  >
+    <span slot="customTitle">Название клиента<br />Адрес </span>
+  </a-table>
 </template>
 <script>
 const columns = [
-	{
-		dataIndex: 'client',
-		key: 'name',
-		width: '25%',
-		slots: { title: 'customTitle' },
-	},
+  {
+    dataIndex: "client",
+    key: "name",
+    width: "25%",
+    slots: { title: "customTitle" },
+  },
 
-	{
-		title: 'Первый из списка почтовый адрес',
-		dataIndex: 'email',
-		width: '25%',
-		key: 'email',
-	},
-	{
-		title: 'Первый из списка номер телефона',
-		dataIndex: 'phone',
-		width: '25%',
-		key: 'phone',
-	},
-	{
-		title: 'Дата создания',
-		dataIndex: 'createdTime',
-		width: '25%',
-		key: 'createdTime',
-	},
+  {
+    title: "Первый из списка почтовый адрес",
+    dataIndex: "email",
+    width: "25%",
+    key: "email",
+  },
+  {
+    title: "Первый из списка номер телефона",
+    dataIndex: "phone",
+    width: "25%",
+    key: "phone",
+  },
+  {
+    title: "Дата создания",
+    dataIndex: "createdTime",
+    width: "25%",
+    key: "createdTime",
+  },
 ];
 export default {
-	props: ['data'],
-	data() {
-		return {
-			columns,
-			dataLength: this.data.length,
-			data1: this.data,
-			dataClient: [],
-		};
-	},
-	mounted() {
-		let clientData = [];
-		let clientName = [];
-		let contactEmailData = [];
-		let contactPhoneData = [];
-		let addressData = [];
-		let createdData = [];
+  props: ["data"],
+  data() {
+    return {
+      columns,
+      dataLength: this.data.length,
+      data1: this.data,
+      dataClient: [],
+    };
+  },
+  mounted() {
+    let clientData = [];
+    let clientName = [];
+    let contactEmailData = [];
+    let contactPhoneData = [];
+    let addressData = [];
+    let addressNumber = [];
+    let createdData = [];
 
-		let editText = (name, address) => {
-			console.log(address);
-			return name + '\n' + address;
-		};
+    const monthList = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
 
-		this.data1.map(client => {
-			clientData.push(client.id);
-			clientName.push(client.name);
-			addressData.push(client.addresses[0].streetName);
-			contactEmailData.push(client.contacts[0].emails[0]);
-			contactPhoneData.push(client.contacts[0].phones[0]);
-			createdData.push(client.createdAt);
-		});
+    let editText = (name, address, addressNumber) => {
+      return name + "\n" + address + " " + addressNumber;
+    };
 
-		for (let i = 0; i < this.dataLength; i++) {
-			this.dataClient.push({
-				id: clientData[i],
-				// name: clientName[i],
-				// address: addressData[i],
-				client: editText(clientName[i], addressData[i]),
-				email: contactEmailData[i],
-				phone: contactPhoneData[i],
-				createdTime: createdData[i],
-			});
-		}
-	},
+    const translateDate = (theDate) => {
+		const time = theDate.toString().slice(11, 16)
+		const dateArr = theDate.toString().slice(0, 10).split("-");
+      const day = dateArr[2];
+      const month = monthList[--dateArr[1]];
+      const year = dateArr[0];
+      return day + " " + month + " " + year + " " + time;
+    };
+
+    this.data1.map((client) => {
+      clientData.push(client.id);
+      clientName.push(client.name);
+      addressData.push(client.addresses[0].streetName);
+      addressNumber.push(client.addresses[0].streetNumber);
+      contactEmailData.push(client.contacts[0].emails[0]);
+      contactPhoneData.push(client.contacts[0].phones[0]);
+      createdData.push(client.createdAt);
+    });
+
+    for (let i = 0; i < this.dataLength; i++) {
+      this.dataClient.push({
+        id: clientData[i],
+        client: editText(clientName[i], addressData[i], addressNumber[i]),
+        email: contactEmailData[i],
+        phone: contactPhoneData[i],
+        createdTime: translateDate(createdData[i]),
+      });
+    }
+  },
 };
 </script>
+
+<style>
+.ant-table-thead > tr > th,
+.ant-table table {
+  text-align: center !important;
+}
+.ant-table-wrapper::before {
+  white-space: normal;
+}
+.ant-table-pagination{
+	display: flex;
+}
+</style>
