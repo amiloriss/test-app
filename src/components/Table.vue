d4f41bc152310fa5cce315e240bb4305145ae62d
georgiy <33293661+amiloriss@users.noreply.github.com>
2021-02-11 12:58:14 +0300

    change logic for output

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,5 +1,9 @@
 <template>
-	<a-table :columns="columns" :data-source="dataClient">
+	<a-table
+		:columns="columns"
+		:data-source="dataClient"
+		:rowKey="record => record.id"
+	>
 		<span slot="customTitle"
 			>Название клиента <br />
 			Адресс
@@ -9,24 +13,29 @@
 <script>
 const columns = [
 	{
-		dataIndex: 'name',
+		dataIndex: 'client',
 		key: 'name',
+		width: '25%',
 		slots: { title: 'customTitle' },
 	},
+
 	{
 		title: 'Первый из списка почтовый адрес',
 		dataIndex: 'email',
+		width: '25%',
 		key: 'email',
 	},
 	{
 		title: 'Первый из списка номер телефона',
-		dataIndex: 'Contacts',
-		key: 'Contacts',
+		dataIndex: 'phone',
+		width: '25%',
+		key: 'phone',
 	},
 	{
 		title: 'Дата создания',
 		dataIndex: 'createdTime',
-		key: 'createdAt',
+		width: '25%',
+		key: 'createdTime',
 	},
 ];
 export default {
@@ -36,63 +45,37 @@ export default {
 			columns,
 			dataLength: this.data.length,
 			data1: this.data,
-			clientData: [],
-			contactEmailData: [],
-			contactPhoneData: [],
-			addressData: [],
-			createdData: [],
 			dataClient: [],
 		};
 	},
 	mounted() {
-		// let client = {
-		// 	clientId: client.id,
-		// 	name: client.name,
-		// 	streetName: client.addresses[0].streetName,
-		// 	email: client.contacts[0].emails[0],
-		// 	phone: client.contacts[0].phones[0],
-		// 	createdTime: client.createdAt,
-		// };
+		let clientData = [];
+		let clientName = [];
+		let contactEmailData = [];
+		let contactPhoneData = [];
+		let addressData = [];
+		let createdData = [];
+
 		this.data1.map(client => {
-			this.clientData.push(client.id);
-			this.addressData.push(client.addresses[0].streetName);
-			this.contactEmailData.push(client.contacts[0].emails[0]);
-			this.contactPhoneData.push({
-				phone: client.contacts[0].phones[0],
-			});
-			this.createdData.push({
-				createdTime: client.createdAt,
-			});
+			clientData.push(client.id);
+			clientName.push(client.name);
+			addressData.push(client.addresses[0].streetName);
+			contactEmailData.push(client.contacts[0].emails[0]);
+			contactPhoneData.push(client.contacts[0].phones[0]);
+			createdData.push(client.createdAt);
 		});
 
 		for (let i = 0; i < this.dataLength; i++) {
 			this.dataClient.push({
-				id: this.clientData[i],
-				address: this.addressData[i],
-				email: this.contactEmailData[i],
+				id: clientData[i],
+				// name: clientName[i],
+				// address: addressData[i],
+				client: clientName[i] + addressData[i],
+				email: contactEmailData[i],
+				phone: contactPhoneData[i],
+				createdTime: createdData[i],
 			});
 		}
-		console.log(this.dataClient);
-
-		// this.data1.map(client => {
-		// 	this.clientData.push({ clientId: client.id, name: client.name });
-		// 	this.addressData.push({
-		// 		addressId: client.addresses[0].clientId,
-		// 		streetName: client.addresses[0].streetName,
-		// 	});
-		// 	this.contactEmailData.push({
-		// 		emailId: client.contacts[0].clientId,
-		// 		email: client.contacts[0].emails[0],
-		// 	});
-		// 	this.contactPhoneData.push({
-		// 		phoneId: client.contacts[0].clientId,
-		// 		phone: client.contacts[0].phones[0],
-		// 	});
-		// 	this.createdData.push({
-		// 		createdId: client.id,
-		// 		createdTime: client.createdAt,
-		// 	});
-		// });
 	},
 };
 </script>
